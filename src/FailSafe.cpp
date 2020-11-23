63f875121a30752ef82982ecb737493e6f4a2775
gmag11 <github@gmartin.net>
2020-11-23 22:37:17 +0100

    Version 0.2.0

--- src/FailSafe.cpp
+++ src/FailSafe.cpp
@@ -1,7 +1,7 @@
 /**
   * @file FailSafe.cpp
-  * @version 0.1.0
-  * @date 11/11/2020
+  * @version 0.2.0
+  * @date 23/11/2020
   * @author German Martin
   * @brief Library to add a simple fail safe mode to any ESP32 or ESP8266 project
   */
